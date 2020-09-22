31fe8cc51de20dc97def57b9b1a22af33b2c756f
fugasi0410 <mayuyu15@i.softbank.jp>
2020-09-22 16:44:56 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :sell_goods
   resources :sell_goods, only: :index
 end
